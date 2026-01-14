d945552c26453fa97a5639f6541d15e49888737f
jwarre-syscall <jwarre46@kent.edu>
2026-01-14 17:02:06 -0500

    some modifications to personal
    in class lecture and practice

--- Personal/Test.py
+++ Personal/Test.py
@@ -1,1 +1,5 @@
 #Creating a test file.
+
+#Testing understanding. print rules, casting, concatination, substring/indexing.
+x = "deshellod3"
+print("hello", "world", end= ", "); print("hello", "world" + " " + str(1+1), end=", "); print(x[3:8], "world", x[9])
